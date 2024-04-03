c1f7cb70f961d42491dab99c8e6bacc262794027
filippodelministro <filippodelministro@gmail.com>
2024-04-03 17:23:19 +0200

    es1 done

--- lab1/ex1.js
+++ lab1/ex1.js
@@ -11,8 +11,8 @@ function Film(id, title, fav = false, watchDate, rating){
 
     this.toString = () => {
         return `Id: ${this.id}, \n` + `Title: ${this.title}, \n` +
-            `Favourite: ${this.fav} \n` + `watchDate: ${this.formatWatchDate('MMMM D, YYYY')}` +
-            `Rating: ${this.formatRating()}`;
+            `Favourite: ${this.fav} \n` + `watchDate: ${this.formatWatchDate('MMMM D, YYYY')}\n` +
+            `Rating: ${this.formatRating()}\n`;
     }
 
     this.formatWatchDate = () => {
@@ -24,3 +24,39 @@ function Film(id, title, fav = false, watchDate, rating){
     }
 }
 
+function FilmLibrary(){
+    this.list = [];
+
+    this.addNewFilm = (film) => {
+        if(!this.list.some(f => f.id === film.id))
+            this.list.push(film);
+        else
+            throw new Error ('Duplicated id!');
+    };
+
+    this.print = () => {
+        console.log("---------- LIST OF FILMS ----------");
+        this.list.forEach((item) => console.log(item.toString()));
+    };
+}
+
+
+function main(){
+    const f1 = new Film(1, "Pulp Fiction", true, "2023-03-10", 5);
+    const f2 = new Film(2, "21 Grams", true, "2023-03-17", 4);
+    const f3 = new Film(3, "Star Wars", false);
+    const f4 = new Film(4, "Matrix");
+    const f5 = new Film(5, "Shrek", false, "2023-03-21", 3);
+
+    const Library = new FilmLibrary();
+    Library.addNewFilm(f1);
+    Library.addNewFilm(f2);
+    Library.addNewFilm(f3);
+    Library.addNewFilm(f4);
+    Library.addNewFilm(f5);
+    
+    Library.print();
+
+}
+
+main();
