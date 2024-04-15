1c271bfa6e0b504a82bf1b9f98509405beeb8cf7
filippodelministro <filippodelministro@gmail.com>
2024-04-15 10:54:34 +0200

    add sqlite 3

--- lab2/ex1.js
+++ lab2/ex1.js
@@ -1,5 +1,7 @@
 
 'use strict';
+
+const sqlite3 = require('sqlite3');
 const dayjs = require("dayjs");
 
 function Film(id, title, fav = false, watchDate, rating){
@@ -25,38 +27,14 @@ function Film(id, title, fav = false, watchDate, rating){
 }
 
 function FilmLibrary(){
-    this.list = [];
+    const db = new sqlite.Database('films.db', (err) => { if (err) throw err; });
 
-    this.addNewFilm = (film) => {
-        if(!this.list.some(f => f.id === film.id))
-            this.list.push(film);
-        else
-            throw new Error ('Duplicated id!');
-    };
 
-    this.print = () => {
-        console.log("---------- LIST OF FILMS ----------");
-        this.list.forEach((item) => console.log(item.toString()));
-    };
 }
 
 
-function main(){
-    const f1 = new Film(1, "Pulp Fiction", true, "2023-03-10", 5);
-    const f2 = new Film(2, "21 Grams", true, "2023-03-17", 4);
-    const f3 = new Film(3, "Star Wars", false);
-    const f4 = new Film(4, "Matrix");
-    const f5 = new Film(5, "Shrek", false, "2023-03-21", 3);
-
-    const Library = new FilmLibrary();
-    Library.addNewFilm(f1);
-    Library.addNewFilm(f2);
-    Library.addNewFilm(f3);
-    Library.addNewFilm(f4);
-    Library.addNewFilm(f5);
-    
-    Library.print();
-
+async function main(){
+    const film = FilmLibrary();
 }
 
 main();
